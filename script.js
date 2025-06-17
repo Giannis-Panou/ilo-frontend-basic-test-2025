e65bcd6fdfe39f70e2e10d6bf04ce8163a514e0d
Giannis Panou <giannispanou500@gmail.com>
2025-06-17 20:11:39 +0300

    Final minor tweaks - Release

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-// Συνάρτηση για fetch των posts και εμφάνιση λίστας
+// Συνάρτηση για fetch 10 post και εμφάνιση λίστας
 async function getPosts() {
 	try {
-		const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const response = await fetch(
+			'https://jsonplaceholder.typicode.com/posts?_limit=10'
+		);
 		const posts = await response.json();
 		const postsDiv = document.getElementById('posts');
 
 		// Καθαρισμός προηγούμενων δεδομένων
 		postsDiv.innerHTML = '';
 
-		// Δημιουργία HTML για κάθε post και  προσθήκη στο postsDiv
+		// Δημιουργία HTML για κάθε post και προσθήκη στο postsDiv
 		const postsList = `
 			<ul id="posts-list">
-				${posts.slice(0, 10).map((post) => `
-					<li onclick="showDetails(this, ${post.id})" class="post">
-						<div class="post-header">
-							<h2>${post.title}</h2>
-							<span class='arrow'> &#11206 </span>
-						</div>
-							<p>${post.body}</p>
-							<div id="details"></div>
-					</li>
-				`).join('')}
+				${posts
+					.map(
+						(post) => `
+						<li onclick="showDetails(this, ${post.id})" class="post">
+							<div class="post-header">
+								<h2>${post.title}</h2>
+								<span class='arrow'> &#11206 </span>
+							</div>
+								<p>${post.body}</p>
+								<div id="details"></div>
+						</li>
+						`
+					)
+					.join('')}
 			</ul>
 		`;
 
@@ -35,7 +41,7 @@ async function showDetails(post, id) {
 	const detailsDiv = post.querySelector('#details');
 	const arrow = post.querySelector('.arrow');
 
-	// Ελέγχουμε αν το detailsDiv είναι ήδη ανοιχτό
+	// Ελέγχουμε αν το detailsDiv είναι ήδη ανοιχτό για να το κλείσουμε
 	if (detailsDiv.classList.contains('active')) {
 		detailsDiv.style.maxHeight = '0';
 		detailsDiv.classList.remove('active');
@@ -57,11 +63,25 @@ async function showDetails(post, id) {
 
 			// Δημιουργία HTML για τα σχόλια
 			detailsDiv.innerHTML = `
-				<h5>Comments:</h5>
+				<div>
+				<h4>Title: </h4>
+				<p> ${post.title}</p>
+				</div>
+				
+				<div>
+				<h4>Body: </h4>
+				<p> ${post.body}</p>
+				</div>
+
+				<h4>Comments:</h4>
 				<ul id="comments-list">
-				${comments.map((comment) =>`
+				${comments
+					.map(
+						(comment) => `
 					<li class='comments'><strong>${comment.name}:</strong> ${comment.body}</li>
-					`).join('')}
+					`
+					)
+					.join('')}
 				</ul>
     		`;
 		} catch (error) {
